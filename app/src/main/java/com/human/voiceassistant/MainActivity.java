73b5a6b4f5784de8fe5f2152dd468d222706e5a9
abidencube14-art <abidencube14@gmail.com>
2026-05-28 17:35:01 +0200

    Update MainActivity.java

--- app/src/main/java/com/human/voiceassistant/MainActivity.java
+++ app/src/main/java/com/human/voiceassistant/MainActivity.java
@@ -1,14 +1,35 @@
 package com.human.voiceassistant;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        Button listenButton = findViewById(R.id.listenButton);
+
+        tts = new TextToSpeech(this, status -> {
+            if(status != TextToSpeech.ERROR) {
+                tts.setLanguage(Locale.US);
+            }
+        });
+
+        listenButton.setOnClickListener(v -> {
+            tts.speak("Hello. I am online and ready.",
+                    TextToSpeech.QUEUE_FLUSH,
+                    null,
+                    null);
+        });
     }
 }
